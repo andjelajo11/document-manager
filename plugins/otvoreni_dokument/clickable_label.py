b74bd1919bb76dde587fb6e71b02adb3e1f3cbc8
Bogdan Suboticki <bogdan.suboticki.19@singimail.rs>
2022-12-26 23:30:34 +0100

    Zavrsene izmene na izgledu preview-a #36

--- plugins/otvoreni_dokument/clickable_label.py
+++ plugins/otvoreni_dokument/clickable_label.py
@@ -7,10 +7,3 @@ class ClickableLabel(QtWidgets.QLabel):
         self.setFrameStyle(QtWidgets.QFrame.StyledPanel | QtWidgets.QFrame.Plain)
         self.setLineWidth(1)
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
-
-    def mousePressEvent(self, event):
-        # Check if the left mouse button was clicked
-        focused_widget = QtWidgets.QApplication.focusWidget()
-        if focused_widget is not None:
-            if event.button() == QtCore.Qt.LeftButton:                
-                print("test")
