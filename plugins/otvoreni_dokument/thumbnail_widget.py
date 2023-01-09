1964e79160828dbbeb62af3b6abe030599b54963
nemanja spasic <fiora.riven@gmail.com>
2023-01-09 17:05:12 +0100

    Napravljeno da komponenta radi sa vise dokumenata u dnosu na workspace koji koristi i da je svesna u thumbnailu koju stranicu trenutno koristi

--- plugins/otvoreni_dokument/thumbnail_widget.py
+++ plugins/otvoreni_dokument/thumbnail_widget.py
@@ -10,59 +10,64 @@ class ThumbnailWidget(QtWidgets.QScrollArea):
         dokument = text
         label_width = 200
         label_height = 150
-        layout = QtWidgets.QGridLayout()
+        self.layout = QtWidgets.QGridLayout()
 
         self.overlay = QtWidgets.QLabel()
-        self.overlay.setText("Selected")
         self.overlay.setAlignment(QtCore.Qt.AlignCenter)
         self.overlay.setStyleSheet("background-color: rgba(128, 128, 128, 0.5); border: 2px solid white")
         self.newLayout = QtWidgets.QHBoxLayout()
         self.newLayout.setContentsMargins(0,0,0,0)
         self.glavni = QtWidgets.QWidget()
         
-        with open("radni_prostor/dokumenti.json", "r") as f:
+        with open("dokumenti/workspace1.json", "r") as f:
             json_data = f.read()
 
         data = json.loads(json_data)
 
         document = data[dokument]
+        print(document[0]["thumbnails"][0].keys())
 
         thumbnails = document[0].get("thumbnails", [])
-
+        id = 0
+        print(thumbnails)
         if len(thumbnails) != 0:
-
-            stranice = thumbnails[0]
+            self.keys = document[0]["thumbnails"][0].keys()
+            print(list(self.keys)[0])
+            self.stranice = thumbnails[id]
 
             # Initialize the row and col variables
             row = 0
             col = 0
 
-            for stranica in stranice:
-                print(stranice[stranica])
-            
-                image1 = QPixmap(stranice[stranica])    
+            for stranica in self.stranice:
+                image1 = QPixmap(self.stranice[stranica])    
                 self.preview_label = ClickableLabel()
                 self.preview_label.setScaledContents(True)
                 self.preview_label.mousePressEvent = self.labelClicked
                 self.preview_label.setFixedSize(label_width, label_height)
                 scaled_image = image1.scaledToWidth(label_width)
                 self.preview_label.setPixmap(scaled_image)
+                
 
-                layout.addWidget(self.preview_label, row, col)
+                self.layout.addWidget(self.preview_label, row, col)
                 row += 1
 
 
-            self.glavni.setLayout(layout)
+            self.glavni.setLayout(self.layout)
 
             self.setWidget(self.glavni)
+
+            if id < len(thumbnails):
+                id += 1
 
 
     def labelClicked(self, event):
         focused_widget = QtWidgets.QApplication.focusWidget()
         if focused_widget is not None:
             if event.button() == QtCore.Qt.LeftButton:                
+                index = self.glavni.layout().indexOf(focused_widget)
                 
-                
+                self.overlay.setText(list(self.keys)[index])
                 focused_widget.setLayout(self.newLayout)
                 self.newLayout.addWidget(self.overlay)
                 self.overlay.setSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
