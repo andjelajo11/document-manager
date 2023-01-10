30563a045bc0999ad772a9381d474a9e44c772ad
andjelajovanovic <andjelapismo@gmail.com>
2023-01-10 19:55:38 +0100

    dodat activity dijagram za komponentu celinaDokument

--- administracija/model/login_model.py
+++
@@ -1,5 +0,0 @@
-class LoginModel:
-    def __init__(self, user_id=None, user_password=None, user_registered=False):
-        self.user_id = user_id
-        self.user_password = user_password
-        self.user_registered = user_registered
--- plugin_framework/dependency.py
+++
@@ -1,4 +0,0 @@
-class Dependency:
-    def __init__(self, _id, version):
-        self.id = _id
-        self.version = version
